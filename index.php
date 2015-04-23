399f7d0e103daa91a667698ea1d3c519194c22e7
Kenneth Prado <Kennethprado45@gmail.com>
2015-04-23 12:02:03 -0700

    2nd To-Do List Video 2.

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-index.php
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Kenneth's 2nd To-Do List</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+	<div class="wrap">
+		
+	</div>
+</body>
+</html>
